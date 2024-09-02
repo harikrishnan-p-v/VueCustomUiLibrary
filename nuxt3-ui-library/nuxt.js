75a2edd0544a98a429873926482d319c35cd616b
CLAYSYS\harikrishnan.vinod <harikrishnan.vinod@claysys.in>
2024-09-03 01:52:05 +0530

    Updated package file

--- nuxt3-ui-library/nuxt.js
+++ nuxt3-ui-library/nuxt.js
@@ -7,7 +7,7 @@ export default defineNuxtModule({
       // Add ./components dir to the list
       dirs.push({
         path: resolve("./components"),
-        prefix: "ui",
+        prefix: "ui-library",
       });
     },
   },
